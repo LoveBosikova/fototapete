f246fef1e59639041df7f45c15292727cc6f1fd7
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2025-01-29 16:53:48 +0300

    howPlaceOrder medium done

--- fototapete/src/components/blocks/howPlaceOrder/howPlaceOrder.tsx
+++ fototapete/src/components/blocks/howPlaceOrder/howPlaceOrder.tsx
@@ -16,28 +16,38 @@ function HowPlaceOrder () {
             <ul className={style.contentWrap}>
                 <li className={style.card1}>
                     <h2 className={style.number}>1:</h2>
-                    <h3 className={style.cardTitle}>{text.step1Title}</h3>
-                    <p className={style.cardText}>{text.step1Text}</p>
+                    <div className={style.cardContent}>
+                        <h3 className={style.cardTitle}>{text.step1Title}</h3>
+                        <p className={style.cardText}>{text.step1Text}</p>
+                    </div>
                 </li>
                 <li className={style.card2}>
                     <h2 className={style.number}>2:</h2>
-                    <h3 className={style.cardTitle}>{text.step2Title}</h3>
-                    <p className={style.cardText}>{text.step2Text}</p>
+                    <div className={style.cardContent}>
+                        <h3 className={style.cardTitle}>{text.step2Title}</h3>
+                        <p className={style.cardText}>{text.step2Text}</p>
+                    </div>
                 </li>
                 <li className={style.card3}>
                     <h2 className={style.number}>3:</h2>
-                    <h3 className={style.cardTitle}>{text.step3Title}</h3>
-                    <p className={style.cardText}>{text.step3Text}</p>
+                    <div className={style.cardContent}>
+                        <h3 className={style.cardTitle}>{text.step3Title}</h3>
+                        <p className={style.cardText}>{text.step3Text}</p>
+                    </div>
                 </li>
                 <li className={style.card4}>
                     <h2 className={style.number}>4:</h2>
-                    <h3 className={style.cardTitle}>{text.step4Title}</h3>
-                    <p className={style.cardText}>{text.step4Text}</p>
+                    <div className={style.cardContent}>
+                        <h3 className={style.cardTitle}>{text.step4Title}</h3>
+                        <p className={style.cardText}>{text.step4Text}</p>
+                    </div>
                 </li>
                 <li className={style.card5}>
                     <h2 className={style.number}>5:</h2>
-                    <h3 className={style.cardTitle}>{text.step5Title}</h3>
-                    <p className={style.cardText}>{text.step5Text}</p>
+                    <div className={style.cardContent}>
+                        <h3 className={style.cardTitle}>{text.step5Title}</h3>
+                        <p className={style.cardText}>{text.step5Text}</p>
+                    </div>
                 </li>
             </ul>
         </section>
