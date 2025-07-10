d9c6b567bbc9b1acd7c3fa16561aec82ddf706dd
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2025-07-10 14:08:16 +0300

    logo - fix

--- src/components/ui/logo/logo.tsx
+++ src/components/ui/logo/logo.tsx
@@ -1,5 +1,7 @@
-import logoWhite from '../../../assets/logo.svg';
-import colorfulLogo from '../../../assets/colorfulLogo.svg';
+// import logoWhite from '../../../assets/logo.svg';
+// import colorfulLogo from '../../../assets/colorfulLogo.svg';
+import { ReactComponent as LogoWhite } from '../../../assets/logo.svg';
+import { ReactComponent as ColorfulLogo } from '../../../assets/colorfulLogo.svg';
 
 import style from './logo.module.scss';
 import { NavLink } from 'react-router-dom';
@@ -10,9 +12,11 @@ type ILogoProps = {
 
 function Logo (props: ILogoProps) {
     const { isWhite } = props
+    const LogoComponent = isWhite ? LogoWhite : ColorfulLogo;
     return(
         <NavLink to='' end>
-                <img className={style.logoImg} src={isWhite? logoWhite : colorfulLogo} alt='Fototapete logo' />
+                {/* <img className={style.logoImg} src={isWhite? logoWhite : colorfulLogo} alt='Fototapete logo' /> */}
+                <LogoComponent className={style.logoImg} aria-label='Fototapete logo' />
         </NavLink>
     ) 
 }
